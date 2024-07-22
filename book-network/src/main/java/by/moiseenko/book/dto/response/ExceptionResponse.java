616abb26b6411319280f40a56b31fc6b52d63e7a
ilyamoiseenko <ilya.moiseenko.00@yandex.by>
2024-07-22 20:18:19 +0300

    Exception handling

---
+++ book-network/src/main/java/by/moiseenko/book/dto/response/ExceptionResponse.java
@@ -0,0 +1,22 @@
+package by.moiseenko.book.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ExceptionResponse {
+
+    private Integer errorCode;
+    private String errorDescription;
+    private String error;
+    private Set<String> validationErrors;
+    private Map<String, String> errors;
+}
