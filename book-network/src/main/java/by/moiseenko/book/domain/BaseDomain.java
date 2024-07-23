0fe52be9bb63216e4ec3ced4c0065843792327b5
ilyamoiseenko <ilya.moiseenko.00@yandex.by>
2024-07-23 21:14:12 +0300

    Create BaseDomain

---
+++ book-network/src/main/java/by/moiseenko/book/domain/BaseDomain.java
@@ -0,0 +1,45 @@
+package by.moiseenko.book.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseDomain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_at", insertable = false)
+    private LocalDateTime lastModifiedAt;
+
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, updatable = false)
+    private Long createdBy;
+
+    @LastModifiedBy
+    @Column(name = "last_modified_at", insertable = false)
+    private Long lastModifiedBy;
+}
